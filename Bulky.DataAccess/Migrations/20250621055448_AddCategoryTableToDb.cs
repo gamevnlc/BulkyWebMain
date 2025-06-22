e888dfb10bd3c2333fc40fdc3621577c71b24e34
Admin <huypx@outlook.com>
2025-06-22 10:42:43 +0700

    005 N-Tier Architecture

--- Bulky.DataAccess/Migrations/20250621055448_AddCategoryTableToDb.cs
+++ Bulky.DataAccess/Migrations/20250621055448_AddCategoryTableToDb.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BulkyWeb.Migrations
+namespace Bulky.DataAccess.Migrations
 {
     /// <inheritdoc />
     public partial class AddCategoryTableToDb : Migration
