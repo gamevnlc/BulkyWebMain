2ec77ac9902a9f68c3df206f88202e5300f3d727
Admin <gamevnlc1591@gmail.com>
2025-06-22 09:13:54 +0700

    005 Display all Categories

--- BulkyWebRazor/Pages/Categories/Index.cshtml.cs
+++ BulkyWebRazor/Pages/Categories/Index.cshtml.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor.Pages.Categories;
 
 public class Index : PageModel
 {
+    private readonly ApplicationDbContext _db;
+    public List<Category> Categories { get; set; }
+
+    public Index(ApplicationDbContext db)
+    {
+        _db = db;
+    }
     public void OnGet()
     {
-        
+        Categories = _db.Categories.ToList();
     }
 }
