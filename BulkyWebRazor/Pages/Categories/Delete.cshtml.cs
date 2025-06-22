eb6aa3f53930f38312a7f18507a8541a36b66015
Admin <gamevnlc1591@gmail.com>
2025-06-22 09:38:51 +0700

    008 Edit and Delete Category

---
+++ BulkyWebRazor/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories;
+
+[BindProperties]
+public class Delete : PageModel
+{
+    private readonly ApplicationDbContext _db;
+
+    // [BindProperty]
+    public Category Category { get; set; }
+
+    public Delete(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int? id)
+    {
+        if (id != null && id != 0)
+        {
+            Category = _db.Categories.Find(id);
+
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        Category? category = _db.Categories.Find(Category.Id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(category);
+        _db.SaveChanges();
+        return RedirectToPage("Index");
+    }
+}
---
+++ BulkyWebRazor/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using BulkyWebRazor.Data;
+using BulkyWebRazor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor.Pages.Categories;
+
+[BindProperties]
+public class Edit : PageModel
+{
+    private readonly ApplicationDbContext _db;
+
+    // [BindProperty]
+    public Category Category { get; set; }
+
+    public Edit(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public void OnGet(int? id)
+    {
+        if (id != null && id != 0)
+        {
+            Category = _db.Categories.Find(id);
+
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(Category);
+            _db.SaveChanges();
+            // TempData["success"] = "The category has been edited successfully";
+            return RedirectToPage("Index");
+        }
+        return Page();
+    }
+}
