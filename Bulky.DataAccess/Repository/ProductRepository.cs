f3267da8460145d6d502de1c4e3fd01deb8e82d6
Admin <gamevnlc1591@gmail.com>
2025-06-24 21:03:54 +0700

    019 Update Product Custom Code

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -15,6 +15,22 @@ public class ProductRepository : Repository<Product>, IProductRepository
     }
     public void Update(Product obj)
     {
-        _db.Products.Update(obj);
+        var objFromDB = _db.Products.FirstOrDefault(u => u.Id == obj.Id);
+        if (objFromDB != null)
+        {
+            objFromDB.Title = obj.Title;
+            objFromDB.Description = obj.Description;
+            objFromDB.Price = obj.Price;
+            objFromDB.CategoryId = obj.CategoryId;
+            objFromDB.ISBN = obj.ISBN;
+            objFromDB.Author = obj.Author;
+            objFromDB.Price50 = obj.Price50;
+            objFromDB.Price100 = obj.Price100;
+            if (obj.ImageUrl != null)
+            {
+                objFromDB.ImageUrl = obj.ImageUrl;
+            }
+        }
+        // _db.Products.Update(obj);
     } 
 }
